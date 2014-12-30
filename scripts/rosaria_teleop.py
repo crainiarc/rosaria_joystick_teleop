82fe9e2578594ab9ac03e0172dede8716fd1e018
Keng Kiat Lim <kkin@crnrc.com>
2014-12-30 17:42:02 +0800

    Parameterised speed limiter

--- scripts/rosaria_teleop.py
+++ scripts/rosaria_teleop.py
@@ -3,15 +3,18 @@ import rospy
 from geometry_msgs.msg import Twist, Vector3
 from sensor_msgs.msg import Joy
 
+speed_limiter = 1
+
 def callback(data):
     pub = rospy.Publisher('RosAria/cmd_vel', Twist)
     cmd_vel = Twist()
-    cmd_vel.linear = Vector3(data.axes[1] / 5, 0, 0)
-    cmd_vel.angular = Vector3(0, 0, data.axes[0] / 5)
+    cmd_vel.linear = Vector3(data.axes[1] / speed_limiter, 0, 0)
+    cmd_vel.angular = Vector3(0, 0, data.axes[0] / speed_limiter)
     pub.publish(cmd_vel)
 
 def joy_relay():
     rospy.init_node('joy_relay', anonymous=True)
+    speed_limiter = rospy.get_param('~speed_limiter', '5')
     rospy.Subscriber('joy', Joy, callback)
     rospy.spin()
 
